4f2d948eaa4d187e2c95c18ba2837457dbf6f350
Danushyan Shandrasekaram <danu.andrasek@gmail.com>
2020-04-22 12:34:04 -0400

    initial commit

---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public static class RandomNumberGenerator
+    {
+        // create Random object (_generator)
+        private static Random _generator = new Random();
+
+        public static int NumberBetween(int minimumValue, int maximumValue)
+        {
+            // user Next() function to get random number
+            return _generator.Next(minimumValue, maximumValue + 1);
+        }
+    }
+}
